27374f279974cbb6f05e4b35c714c020ed89d329
brdn <=>
2024-12-21 18:45:24 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const fetchBookmarks = async () => {
+    try {
+        const response = await fetch(
+            "https://brdnsbookmarkapi.netlify.app/.netlify/functions/fetch-bookmarks",
+            { method: "POST" }
+        );
+        const data = await response.json();
+        console.log("Fetched Bookmarks:", data.bookmarks);
+    } catch (error) {
+        console.error("Error fetching bookmarks:", error);
+    }
+};
+
+fetchBookmarks();
