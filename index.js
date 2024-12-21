297080e8f2ec65517584067b2fc76283df5af398
nolongerbrdn <97868274+nolongerbrdn@users.noreply.github.com>
2024-12-21 04:22:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const booksContainer = document.querySelector('.bookmarks');
+
+fetch('data/books.json') // Replace 'manga.json' with 'books.json'
+    .then((response) => response.json())
+    .then((books) => {
+        books.forEach((book) => {
+            const bookDiv = document.createElement('div');
+            bookDiv.classList.add('bookmark');
+            bookDiv.innerHTML = `
+                    <a href="${book.link}" target="_blank">
+                        <img src="${book.cover}" alt="${book.title} Cover" />
+                        <p>${book.title}</p>
+                    </a>
+                `;
+            booksContainer.appendChild(bookDiv);
+        });
+    })
+    .catch((error) => console.error('Error loading book data:', error));
+
