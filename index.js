32427dacafe7bb36bc4b4f172a43b8e5b0535867
brdn <=>
2024-12-21 05:41:25 -0500

    Change index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-fetch('https://fastidious-mousse-e02a5c.netlify.app/api/hello')
+fetch('https://fastidious-mousse-e02a5c.netlify.app/api/hello', {
+    mode: 'cors'
+})
     .then(response => response.json())
     .then(data => console.log(data.message))  // Logs "Hello from the backend!"
     .catch(error => console.error('Error fetching data:', error));
